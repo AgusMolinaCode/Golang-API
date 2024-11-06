d66c5b6bf08951cbba928f4393868e0e650a4fa4
Agustin <am.motos@hotmail.com>
2024-11-06 15:45:35 -0300

    Add JWT authentication and JSON schema validation for user registration and login

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/AgusMolinaCode/Golang-API.git
 go 1.23.2
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/justinas/alice v1.2.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/justinas/alice"
 	_ "github.com/lib/pq"
+	"github.com/xeipuuv/gojsonschema"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type App struct {
-	DB *sql.DB
+	DB     *sql.DB
+	JWTKey []byte
 }
 
 type Credentials struct {
@@ -26,6 +33,7 @@ type Credentials struct {
 type UserResponse struct {
 	XataID   string `json:"xata_id"`
 	Username string `json:"username"`
+	Token    string `json:"token,omitempty"`
 }
 
 type ErrorResponse struct {
@@ -37,11 +45,31 @@ type RouteMessage struct {
 	ID      string `json:"id,omitempty"`
 }
 
+type Claims struct {
+	Username string `json:"username"`
+	XataID   string `json:"xata_id"`
+	jwt.RegisteredClaims
+}
+
 func main() {
 
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	var loadErr error
+	userSchema, loadErr := loadSchema("schemas/user.json")
+
+	if loadErr != nil {
+		log.Fatal(loadErr)
+	}
+
+
+
+	JWTKey := []byte(os.Getenv("JWT_SECRET"))
+	if len(JWTKey) == 0 {
+		log.Fatal("Error environment variable")
 	}
 
 	connString := os.Getenv("XATA_PSQL_URL")
@@ -61,8 +89,10 @@ func main() {
 
 	chain := alice.New(logginMiddleware).Then(router)
 
-	router.HandleFunc("/register", register).Methods("POST")
-	router.HandleFunc("/login", login).Methods("POST")
+	userChain:= alice.New(logginMiddleware, validateMiddleware(userSchema)).Then(router)
+
+	router.HandleFunc("/register", app.register).Methods("POST")
+	router.HandleFunc("/login", app.login).Methods("POST")
 	router.HandleFunc("/project", createProject).Methods("POST")
 	router.HandleFunc("/project/{id}", updateProject).Methods("PUT")
 	router.HandleFunc("/project/{id}", getProject).Methods("GET")
@@ -74,6 +104,16 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+
+// LoadSchema function to load schema from file
+func loadSchema(filePath string) (string, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
 func logginMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
@@ -81,10 +121,107 @@ func logginMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func (app *App) jwtMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			respondWithError(w, http.StatusUnauthorized, "Unauthorized")
+			return
+		}
+
+		claims := &Claims{}
+
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return app.JWTKey, nil
+		})
+
+		if err != nil {
+			respondWithError(w, http.StatusUnauthorized, "Unauthorized")
+			return
+		}
+
+		if !token.Valid {
+			respondWithError(w, http.StatusUnauthorized, "Unauthorized")
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "claims", claims)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func validateMiddleware(schema string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var body map[string]interface{}
+
+			bodyBytes, err := io.ReadAll(r.Body)
+			if err != nil {
+				respondWithError(w, http.StatusInternalServerError, "Error reading request")
+				return
+			}
+
+			err = json.Unmarshal(bodyBytes, &body)
+			if err != nil {
+				respondWithError(w, http.StatusBadRequest, "Invalid request")
+				return
+			}
+
+			schemaLoader := gojsonschema.NewStringLoader(schema)
+
+			documentLoader := gojsonschema.NewGoLoader(body)
+
+			result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+
+			if err != nil {
+				respondWithError(w, http.StatusInternalServerError, "Error validating JSON")
+				return
+			}
+
+			if !result.Valid() {
+				var errs []string
+				for _, err := range result.Errors() {
+					errs = append(errs, err.String())
+				}
+				respondWithError(w, http.StatusBadRequest, "Invalid request: "+strings.Join(errs, ", "))
+			}
+
+			r.Body = io.NopCloser(strings.NewReader(string(bodyBytes)))
+
+			next.ServeHTTP(w, r)
+
+		})
+	}
+}
+
 func respondWithError(w http.ResponseWriter, code int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(ErrorResponse{Message: message})
+}
+
+func (app *App) createToken(xataID, username string) (string, error) {
+	expirationTime := time.Now().Add(5 * time.Minute)
+
+	claims := &Claims{
+		Username: username,
+		XataID:   xataID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(app.JWTKey)
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+
 }
 
 // Register function to handle registration
@@ -107,13 +244,62 @@ func (app *App) register(w http.ResponseWriter, r *http.Request) {
 	var xataID string
 	err = app.DB.QueryRow("INSERT INTO users (username, password) VALUES ($1, $2) RETURNING xata_id", creds.Username, string(hashedPassword)).Scan(&xataID)
 
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error inserting user")
+		return
+	}
+
+	tokenString, err := app.createToken(xataID, creds.Username)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating token")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(UserResponse{XataID: xataID, Username: creds.Username, Token: tokenString})
 
 }
 
 // Login
-func login(w http.ResponseWriter, r *http.Request) {
+func (app *App) login(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request")
+		return
+	}
+
+	var xataID string
+	var storedCreds Credentials
+
+	err = app.DB.QueryRow("SELECT xata_id, username, password FROM users WHERE username = $1", creds.Username).Scan(&xataID, &storedCreds.Username, &storedCreds.Password)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			respondWithError(w, http.StatusUnauthorized, "Invalid username or password")
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, "Error querying database")
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password))
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Invalid credentials")
+		return
+	}
+
+	tokenString, err := app.createToken(xataID, creds.Username)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating token")
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(RouteMessage{Message: "Login"})
+	json.NewEncoder(w).Encode(UserResponse{XataID: xataID, Username: creds.Username, Token: tokenString})
+
 }
 
 // Create Project
